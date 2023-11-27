65f6016b3967b8aebfdaf0192a74646b1e6cf9ae
Tori Enyart <torienyart@gmail.com>
2023-11-27 12:56:51 -0500

    Attempt fixes for SSL Faraday error

--- app/services/parks_service.rb
+++ app/services/parks_service.rb
@@ -1,14 +1,31 @@
+# class ParksService
+#   def self.get_parks_by_state(state)
+#     get_url("/parks?q=#{state}&api_key=#{ENV['NPS_API_KEY']}")
+#   end
+
+#   def self.get_url(url)
+#     response = conn.get(url)
+#     JSON.parse(response.body, symbolize_names: true)
+#   end
+
+#   def self.conn
+#     Faraday.new(url: "https://developer.nps.gov/api/v1")
+#   end
+# end
+
 class ParksService
-  def get_parks_by_state(state)
-    get_url("parks?q=#{state}&api_key=#{ENV['NPS_API_KEY']}")
+  def self.conn
+    Faraday.new(url: "https://developer.nps.gov/api/v1/") do |faraday|
+      faraday.headers["x-api-key"] = ENV["NPS_API_KEY"]
+    end
   end
 
-  def get_url(url)
+  def self.get_url(url)
     response = conn.get(url)
     JSON.parse(response.body, symbolize_names: true)
   end
 
-  def conn
-    Faraday.new(url: "curl -X GET "https://developer.nps.gov/api/v1/")
+  def self.get_parks_by_state(state_code)
+    get_url("parks?stateCode=#{state_code}")
   end
 end
--- spec/services/parks_service_spec.rb
+++ spec/services/parks_service_spec.rb
@@ -4,11 +4,13 @@ describe 'ParksService' do
     it 'returns parks by state' do
       tennessee_parks = ParksService.get_parks_by_state('TN')
 
+      require 'pry'; binding.pry
+
       expect(tennessee_parks).to be_a(Hash)
       expect(tennessee_parks).to have_key(:total)
       expect(tennessee_parks[:total]).to be_an(Integer)
       expect(tennessee_parks[:data]).to be_an(Array)
-      expect(tennessee_parks[:data].first).to be_a(Hash)!!
+      expect(tennessee_parks[:data].first).to be_a(Hash)
     end
   end
 end
